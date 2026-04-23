b58f9c479296cad5e58970c02dedb26f980214fa
Humming-Moon-afk <milian.loechner@gmail.com>
2026-04-23 10:28:20 +0200

    feat: added static ID that increases +1 for every user to later find them easier

--- Backend/Patient.java
+++ Backend/Patient.java
@@ -4,9 +4,11 @@ public class Patient {
     private String name;
     private int age;
     private int height;
+    private static int ID;
     public Patient(String name, int age, int height) {
         this.name = name;
         this.age = age;
         this.height = height;
+        ID++;
     }
 }
